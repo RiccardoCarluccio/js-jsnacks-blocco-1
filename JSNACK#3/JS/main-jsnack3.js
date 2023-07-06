37e9903caa11a1a60c8b051c2af8ffe367053817
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-06 17:44:23 +0200

    aggiunto paragone degli array jsnack#3

--- JSNACK#3/JS/main-jsnack3.js
+++ JSNACK#3/JS/main-jsnack3.js
@@ -1,8 +1,18 @@
 const gorillazArray = ['She\'s my Collar', 'Cracker Island', 'Dirty Harry', 'El Manana', 'Feel Good Inc.', 'Clint Eastwood', 'Saturn Barz'];
 const museArray = ['New Born', 'Bliss', 'Plug in Baby', 'Citizen Erased', 'Micro Cuts'];
 
-for (i = museArray.length; i < gorillazArray.length; i++) {
-  museArray.push(gorillazArray[Math.floor(Math.random() * 7)])
+if (gorillazArray.length < museArray.length) {
+  for (i = gorillazArray.length; i < museArray.length; i++) {
+    museArray.push(museArray[Math.floor(Math.random() * 7)])
 
-  console.log(museArray);
+    console.log(gorillazArray);
+  }
+} else if (museArray.length < gorillazArray.length) {
+    for (i = museArray.length; i < gorillazArray.length; i++) {
+      museArray.push(gorillazArray[Math.floor(Math.random() * 7)])
+
+      console.log(museArray);
+    }
+} else {
+    console.log('I due array hanno pari lunghezza');
 }

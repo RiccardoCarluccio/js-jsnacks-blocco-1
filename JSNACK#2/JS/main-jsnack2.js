fdd8ff734a6bb2d17f4a104259af8997aa3c4571
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-06 17:25:42 +0200

    random number generator to fill the array working

---
+++ JSNACK#2/JS/main-jsnack2.js
@@ -0,0 +1,13 @@
+const arrayOfNumbers = [];
+
+for (i = 0; i < 10; i++) {
+  const arrayItem = Math.floor(Math.random() * 11);
+  console.log(`Random generated number: ${arrayItem}`);
+
+  arrayOfNumbers.push(arrayItem);
+  console.log(`The random generated array is: ${arrayOfNumbers}`);
+
+  if (i === 10) {   //forse mettere 9
+    const sumOfOddsPlaced = 
+  }
+}

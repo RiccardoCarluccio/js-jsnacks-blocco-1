53dcac49afe65e731bf12f7141490aff57df44df
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-06 17:30:33 +0200

    Secondo esercizio finito

--- JSNACK#2/JS/main-jsnack2.js
+++ JSNACK#2/JS/main-jsnack2.js
@@ -7,7 +7,13 @@ for (i = 0; i < 10; i++) {
   arrayOfNumbers.push(arrayItem);
   console.log(`The random generated array is: ${arrayOfNumbers}`);
 
-  if (i === 10) {   //forse mettere 9
-    const sumOfOddsPlaced = 
+  if (i === 9) {   //forse mettere 9
+    let sumOfOddsPlaced = 0;
+    for (j = 0; j < arrayOfNumbers.length; j++) {
+      if (j % 2 !== 0) {
+        sumOfOddsPlaced += parseInt(arrayOfNumbers[j]);
+        console.log(sumOfOddsPlaced);
+      }
+    }
   }
 }
